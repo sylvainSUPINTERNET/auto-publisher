c5013bf25b5e6782408432d9c18f975905d21246
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-11-15 16:39:31 +0100

    fix(oauth2): scopes

--- app/api/oauth2/tiktok/authorization/route.ts
+++ app/api/oauth2/tiktok/authorization/route.ts
@@ -14,7 +14,7 @@ export async function GET( req: NextRequest ) {
 
     let url = 'https://www.tiktok.com/v2/auth/authorize/';
     url += `?client_key=${process.env.OAUTH2_TIKTOK_CLIENT_ID}`;
-    url += '&scope=user.info.basic';
+    url += '&scope=user.info.basic,video.list,video.publish,video.upload,user.info.profile,user.info.stats';
     url += '&response_type=code';
     url += `&redirect_uri=${process.env.OAUTH2_TIKTOK_REDIRECT_URI}`;
     url += '&state=' + csrfState;
